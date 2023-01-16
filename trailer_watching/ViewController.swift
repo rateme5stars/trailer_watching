94a28fd5c244dcb18f52abc568cd78da0c378727
rateme5stars <dzungardan@gmail.com>
2023-01-16 13:23:32 +0700

    Initial Commit

---
+++ trailer_watching/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trailer_watching
+//
+//  Created by Dzung Ngo on 16/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
